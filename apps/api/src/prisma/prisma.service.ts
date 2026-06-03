a090dd0ce94250183c27e4f721046fb21e372b01
Ahmed Wagdy <ahmedwa122@gmail.com>
2026-06-03 20:21:19 +0300

    refactor: Simplify PrismaService constructor by removing database pool and adapter

--- apps/api/src/prisma/prisma.service.ts
+++ apps/api/src/prisma/prisma.service.ts
@@ -1,21 +1,15 @@
 import { Injectable, OnModuleDestroy } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
-import { PrismaPg } from '@prisma/adapter-pg';
-import { Pool } from 'pg';
-import { AppConfigService } from '../config/config.service';
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleDestroy {
-  private readonly pool: Pool;
-
-  constructor(config: AppConfigService) {
-    const pool = new Pool({ connectionString: config.databaseUrl });
-    const adapter = new PrismaPg(pool);
-    super({ adapter });
-    this.pool = pool;
+  constructor() {
+    super({
+      log: process.env.NODE_ENV === 'development' ? ['query', 'error', 'info', 'warn'] : ['error'],
+    });
   }
 
   async onModuleDestroy() {
-    await this.pool.end();
+    await this.$disconnect();
   }
 }
